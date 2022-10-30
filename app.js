2e477a4733b67e91e2d0be8ed0082c9196b6ec5f
semilore <semilore@omotade-michaels.com>
2022-10-30 14:42:52 +0100

    test

--- app.js
+++ app.js
@@ -5,14 +5,23 @@ class Calculator {
   }
 
   clear() {
-    (this.currentOp = ""), (this.prevOp = ""), (this.operation = undefined);
+    this.currentOp = "";
+    this.prevOp = "";
+    this.operation = undefined;
   }
 
   delete() {}
 
-  append() {}
+  appendNum(number) {
+    if (number === "." && this.currentOp.includes(".")) return;
+    this.currentOp = this.currentOp.toString() + number.toString();
+  }
 
-  chooseOperation(operation) {}
+  chooseOperation(operation) {
+    this.operation = operation;
+    this.prevOp = this.prevOp;
+    this.currentOp = " ";
+  }
   compute() {}
 
   updateDisplay() {
@@ -20,19 +29,26 @@ class Calculator {
   }
 }
 
-const numberButtons = document.querySelectorAll("[data-number]");
-const operationButtons = document.querySelectorAll("[data-operation]");
-const equalsButton = document.querySelector("[data-equals]");
-const deleteButton = document.querySelector("[data-delete]");
-const allClearButton = document.querySelector("[data-all-clear]");
-const prevOpText = document.querySelector("[data-prev-op]");
-const currentOpText = document.querySelector("[data-current-op]");
+const numberButtons = document.querySelectorAll("$data-number");
+const operationButtons = document.querySelectorAll("#data-operation");
+const equalsButton = document.querySelector("#data-equals");
+const deleteButton = document.querySelector("#data-delete");
+const allClearButton = document.querySelector("#data-all-clear");
+const prevOpText = document.querySelector("#data-prev-op");
+const currentOpText = document.querySelector("#data-current-op");
 
 const calculator = new Calculator(prevOpText, currentOpText);
 
 numberButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    calculator.appendNumber(button.innerText);
+    calculator.appendNum(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
     calculator.updateDisplay();
   });
 });
