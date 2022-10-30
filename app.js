24129f51cfb30402f242009102db91f0d5ed2d20
semilore <semilore@omotade-michaels.com>
2022-10-30 17:11:24 +0100

    test

--- app.js
+++ app.js
@@ -5,16 +5,41 @@ const allSymbols = ["+", "-", "x", "%", "AC", "=", "&#247;"];
 let firstValue = "";
 let secondValue = "";
 let symbol = "";
+let result = "";
+
+const calculate = () => {
+  firstValue = parseFloat(firstValue);
+  secondValue = parseFloat(secondValue);
+
+  if (symbol === "+") result = firstValue + secondValue;
+  if (symbol === "-") result = firstValue - secondValue;
+  if (symbol === "x") result = firstValue * secondValue;
+  if (symbol === "&#247;") result = firstValue / secondValue;
+  if (symbol === "%") result = firstValue % secondValue;
+
+  display.innerText = result;
+  firstValue = result;
+  secondValue = "";
+};
 
 for (let button of controlButtons) {
   button.addEventListener("click", () => {
     const { innerText: btnValue } = button;
     const btnValueIsSymbol = allSymbols.includes(btnValue);
 
+    if (!secondValue && btnValue === "=") return null;
+    if (btnValue === "AC") {
+      firstValue = "";
+      secondValue = "";
+      symbol = "";
+      return (display.innerText = "");
+    }
+
     if (firstValue && btnValueIsSymbol) {
+      secondValue && calculate();
       symbol = btnValue;
     } else if (!symbol) {
-      firstValue = btnValue;
+      firstValue += btnValue;
     } else if (symbol) {
       secondValue += btnValue;
     }
